2956eb2023f261c97b5daeb619c97fbd4cf46980
JoseEnri <jose.tocasuche@gmail.com>
2025-06-11 21:43:52 +0000

    completed excuses java-jet

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
+let who = ['My boss', 'My mother-in-law', 'The mailman', 'My bird', 'my ex-wife'];
+let action = ['ate', 'peed on', 'crushed', 'broke', 'chewed', 'wrecked'];
+let what = ['my homework', 'my phone', 'the car', 'my money', 'my marriage'];
+let when = ['before class', 'while I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function myRandomItem (min,max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+function myRandom(anyArray) {
+  let max = anyArray.length - 1;
+  let min = 0;
+  let random = myRandomItem (min,max);
+  return anyArray[random];
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("This is a test!!");
+  document.querySelector ('#excuse').innerHTML = myRandom(who) + " " + myRandom(action) + " " + myRandom(what) + " " + myRandom(when)
 };
